a81a5d955616582cddfa17709311b5f1de167647
AnthonyMarche <marche.anthony72@gmail.com>
2023-10-06 13:47:19 +0200

    admin show and delete animals

--- resources/js/components/animal/ShowAnimals.vue
+++ resources/js/components/animal/ShowAnimals.vue
@@ -3,6 +3,7 @@ import CardShow from "@/components/animal/CardShow.vue";
 import SelectInput from "@/components/SelectInput.vue";
 import {ref, toRefs, watch, watchEffect} from "vue";
 import InputLabel from "@/components/InputLabel.vue";
+import {usePage} from "@inertiajs/vue3";
 
 const props = defineProps({
   types: {
@@ -16,6 +17,7 @@ const props = defineProps({
 const selectedType = ref(null);
 const selectedBreed = ref(null);
 const selectedOrderBy = ref('created_at');
+const selectedSaleStatus = ref('')
 const emit = defineEmits(['updateAnimals']);
 const animals = ref([]);
 const {breeds} = toRefs(props);
@@ -25,6 +27,12 @@ const orderByOptions = {
   name: "Nom",
   price_HT: "Prix"
 };
+const saleStatusOptions = {
+  '': "Tous",
+  1: "En vente",
+  0: "Vendu",
+};
+const user = usePage().props.auth.user;
 
 watch(selectedType, (newType) => {
   selectedBreed.value = null;
@@ -44,11 +52,17 @@ const fetchFilteredAnimals = () => {
     params: {
       selectedType: selectedType.value,
       selectedBreed: selectedBreed.value,
-      selectedOrderBy: selectedOrderBy.value
+      selectedOrderBy: selectedOrderBy.value,
+      selectedSaleStatus: selectedSaleStatus.value
     },
   })
       .then(response => {
-        animals.value = response.data.animals;
+        if (user) {
+          animals.value = response.data.animals;
+        } else {
+          animals.value = response.data.animals.filter(
+              animal => animal.sale_status === 'en vente');
+        }
         emit('updateAnimals', animals.value);
       })
       .catch(error => {
@@ -63,9 +77,9 @@ watchEffect(() => {
 
 <template>
     <div class="flex mt-5">
-      <div class="flex justify-center ml-14 w-1/2">
-        <div class="flex items-center mr-5">
-          <InputLabel class="mr-5" for="type" value="Type :"/>
+      <div class="flex justify-center w-1/2">
+        <div class="flex items-center px-6">
+          <InputLabel class="mr-4" for="type" value="Type :"/>
           <SelectInput
               id="type"
               :options="types"
@@ -73,27 +87,35 @@ watchEffect(() => {
               v-model="selectedType"
           />
         </div>
-        <div class="flex items-center">
-          <InputLabel class="mr-5" for="breed" value="Race :"/>
+        <div class="flex items-center px-6">
+          <InputLabel class="mr-4" for="breed" value="Race :"/>
           <SelectInput
               id="breed"
               :options="validBreed"
               default-option="Toutes les races"
               v-model="selectedBreed"/>
         </div>
+        <div v-if="user" class="flex items-center">
+          <InputLabel class="mr-4" for="sale_status" value="Status de vente :"/>
+          <select id="sale_status" v-model="selectedSaleStatus">
+            <option v-for="(label, value) in saleStatusOptions" :value="value">
+              {{ label }}
+            </option>
+          </select>
+        </div>
       </div>
-      <div class="w-1/2 flex items-center justify-center">
-        <InputLabel class="mr-5" for="orderBy" value="Trier par :"/>
-        <select v-model="selectedOrderBy">
+      <div class="w-1/2 flex items-center justify-end px-28">
+        <InputLabel class="mr-4" for="orderBy" value="Trier par :"/>
+        <select id="orderBy" v-model="selectedOrderBy">
           <option v-for="(label, value) in orderByOptions" :value="value">
             {{ label }}
           </option>
         </select>
       </div>
     </div>
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 mt-10">
+    <div class="card mx-auto sm:px-6 lg:px-8 space-y-6 mt-10">
       <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg" v-for="animal in animals">
-        <CardShow :animal="animal"></CardShow>
+        <CardShow :animal="animal" @refreshAnimals="fetchFilteredAnimals"></CardShow>
       </div>
       <div v-if="!animals.length" class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
         <p class="text-center">Aucun animal ne correspond à vos critères</p>
@@ -102,4 +124,7 @@ watchEffect(() => {
 </template>
 
 <style>
+.card {
+  max-width: 95rem;
+}
 </style>
