3e7b0402e54142cc94e6a3a7c8c983d0c16d9bc0
AnthonyMarche <marche.anthony72@gmail.com>
2023-10-21 18:15:57 +0200

    refacto breed back and front using Inertia

--- resources/js/components/Breed/BreedForm.vue
+++ resources/js/components/Breed/BreedForm.vue
@@ -6,8 +6,7 @@ import InputError from "@/components/InputError.vue";
 import InputLabel from "@/components/InputLabel.vue";
 import SecondaryButton from "@/components/SecondaryButton.vue";
 import {useToast} from "vue-toastification";
-import {toRefs} from "vue";
-import SelectInput from "@/components/SelectInput.vue";
+import {computed, toRefs} from "vue";
 
 const props = defineProps({
     types: {
@@ -19,40 +18,47 @@ const props = defineProps({
     }
 });
 
-const {breed} = toRefs(props);
+const {types} = toRefs(props);
 const toast = useToast();
 const form = useForm({
-    name: breed.value ? breed.value.name : '',
-    type_id: breed.value ? breed.value.type.id : null
+    name: props.breed?.name ?? '',
+    type_id: props.breed?.type.id ?? null
+});
+
+const typesOption = computed(() => {
+    if (props.breed) {
+        return types.value.filter(
+            type => type.name !== props.breed.type.name
+        );
+    }
+
+    return props.types;
+});
+
+const defaultType = computed(() => {
+    if (props.breed) {
+        return props.breed.type;
+    }
+
+    return {
+        name: 'Selectionnez un type',
+        id: null
+    }
 });
 
 const submit = () => {
-    if (breed.value) {
-        axios.put(route('breeds.update', {id: breed.value.id}), form)
-            .then(() => {
-                toast.success("Race modifiée avec succès")
-                router.visit(route('breeds.index'))
-            })
-            .catch(error => {
+    if (props.breed) {
+        form.put(route('breeds.update', {breed: props.breed.id}), {
+            onError: () => {
                 toast.error("Erreur lors de la modification")
-                const errors = error.response.data.errors;
-                for (const field in errors) {
-                    form.errors[field] = errors[field][0];
-                }
-            })
+            },
+        });
     } else {
-        axios.post(route('breeds.store'), form)
-            .then(() => {
-                toast.success("Race créée avec succès")
-                router.visit(route('breeds.index'))
-            })
-            .catch(error => {
+        form.post(route('breeds.store'), {
+            onError: () => {
                 toast.error("Erreur lors de la création")
-                const errors = error.response.data.errors;
-                for (const field in errors) {
-                    form.errors[field] = errors[field][0];
-                }
-            })
+            },
+        });
     }
 };
 </script>
@@ -60,31 +66,34 @@ const submit = () => {
 <template>
     <form @submit.prevent="submit" class="flex flex-col items-center py-8">
         <div class="w-1/2">
-            <InputLabel for="type" value="Type"/>
-            <select-input
+            <InputLabel for="type" value="Type d'animal "/>
+            <select
                 v-model="form.type_id"
                 id="type"
                 class="mt-1 block w-full"
-                default-option="Selectionnez un type"
-                :options="types"
-            />
+                autofocus
+            >
+                <option :value="defaultType.id" selected>{{ defaultType.name }}</option>
+                <option v-for="type in typesOption" :value="type.id">
+                    {{ type.name }}
+                </option>
+            </select>
             <InputError class="mt-2" :message="form.errors.type_id"/>
 
-            <InputLabel for="name" value="Nom de la race" class="mt-4"/>
+            <InputLabel for="name" value="Race d'animal" class="mt-4"/>
             <TextInput
                 id="name"
                 type="text"
                 class="mt-1 block w-full"
                 v-model="form.name"
                 required
-                autofocus
-                autocomplete="name"
+                autocomplete="on"
             />
             <InputError class="mt-2" :message="form.errors.name"/>
         </div>
         <div class="flex items-center justify-end mt-6 space-x-8">
             <SecondaryButton @click="router.visit(route('breeds.index'))">Annuler</SecondaryButton>
-            <PrimaryButton>Enregistrer</PrimaryButton>
+            <PrimaryButton type="submit" :disabled="form.processing">Enregistrer</PrimaryButton>
         </div>
     </form>
 </template>
